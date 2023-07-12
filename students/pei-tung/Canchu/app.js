5c5a2a513175ac22e9f6ad98ac274ccb902b69f9
PTC1116 <ptchen1116jslearning@gmail.com>
2023-07-12 11:29:54 +0800

    debug

--- students/pei-tung/Canchu/app.js
+++ students/pei-tung/Canchu/app.js
@@ -10,12 +10,12 @@ const userRoutes = require("./server/routes/userRoute");
 const friendRoutes = require("./server/routes/friendRoute");
 const eventRoutes = require("./server/routes/eventRoutes");
 
+app.use(express.json());
+app.use(express.static("public"));
+
 app.use("/api/1.0/users", userRoutes);
 app.use("/api/1.0/friends", friendRoutes);
 app.use("/api/1.0/events", eventRoutes);
-
-app.use(express.json());
-app.use(express.static("public"));
 
 app.listen(80, () => {
   console.log("Server started on port 80");
