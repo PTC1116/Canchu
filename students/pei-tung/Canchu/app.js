69de96fe9e757b86a61f2109ef9594f9801e201c
PTC1116 <ptchen1116jslearning@gmail.com>
2023-07-17 23:13:32 +0800

    try to handle CORS-related issues

--- students/pei-tung/Canchu/app.js
+++ students/pei-tung/Canchu/app.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
@@ -11,6 +12,7 @@ const friendRoutes = require("./server/routes/friendRoute");
 const eventRoutes = require("./server/routes/eventRoutes");
 const postRoutes = require("./server/routes/postRoutes");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
