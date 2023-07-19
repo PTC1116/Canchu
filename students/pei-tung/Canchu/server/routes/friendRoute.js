f762bc2d03e22435414e394a7092606905e9a295
PTC1116 <ptchen1116jslearning@gmail.com>
2023-07-19 18:17:48 +0800

    test pending API

--- students/pei-tung/Canchu/server/routes/friendRoute.js
+++ students/pei-tung/Canchu/server/routes/friendRoute.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const friendController = require("../controllers/friendController");
-const friendUtil = require("../../util/friendUtil");
-const auth = require("../../util/auth");
+const friendController = require('../controllers/friendController');
+const friendUtil = require('../../util/friendUtil');
+const auth = require('../../util/auth');
 
-router.get("/", auth.auth, friendController.showAllFriends);
-router.get("/pending", auth.auth, friendController.pending);
-router.post("/:user_id/request", auth.auth, friendController.request);
-router.post("/:friendship_id/agree", auth.auth, friendController.agree);
-router.delete("/:friendship_id", auth.auth, friendController.delete);
+// router.get("/", auth.auth, friendController.showAllFriends);
+router.get('/pending', auth.auth, friendController.pending);
+router.post('/:user_id/request', auth.auth, friendController.request);
+router.post('/:friendship_id/agree', auth.auth, friendController.agree);
+router.delete('/:friendship_id', auth.auth, friendController.delete);
 module.exports = router;
