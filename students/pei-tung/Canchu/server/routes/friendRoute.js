2a154962a02ab27ff3d8fea438fc5d42bfe74a1f
PTC1116 <ptchen1116jslearning@gmail.com>
2023-07-19 18:25:25 +0800

    test event and pending API

--- students/pei-tung/Canchu/server/routes/friendRoute.js
+++ students/pei-tung/Canchu/server/routes/friendRoute.js
@@ -4,7 +4,7 @@ const friendController = require('../controllers/friendController');
 const friendUtil = require('../../util/friendUtil');
 const auth = require('../../util/auth');
 
-router.get('/', auth.auth, friendController.showAllFriends);
+//router.get('/', auth.auth, friendController.showAllFriends);
 router.get('/pending', auth.auth, friendController.pending);
 router.post('/:user_id/request', auth.auth, friendController.request);
 router.post('/:friendship_id/agree', auth.auth, friendController.agree);
