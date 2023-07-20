a52891fb547c34dcd0aaa6209ec83890d00819ae
PTC1116 <ptchen1116jslearning@gmail.com>
2023-07-20 17:57:49 +0800

    put redis related code to util

--- students/pei-tung/Canchu/app.js
+++ students/pei-tung/Canchu/app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const { createClient } = require('redis');
 
 const app = express();
 const userRoutes = require('./server/routes/userRoute');
 const friendRoutes = require('./server/routes/friendRoute');
 const eventRoutes = require('./server/routes/eventRoutes');
 const postRoutes = require('./server/routes/postRoutes');
-const redis = require('redis');
 
 app.use(cors());
 app.use(express.json());
@@ -18,17 +16,6 @@ app.use('/api/1.0/users', userRoutes);
 app.use('/api/1.0/friends', friendRoutes);
 app.use('/api/1.0/events', eventRoutes);
 app.use('/api/1.0/posts', postRoutes);
-
-const client = createClient();
-client.on('connect', () => {
-  console.log('Connected to Redis');
-});
-
-client.on('error', (err) => {
-  console.error('Redis error:', err);
-});
-
-client.connect();
 
 const port = 3000;
 app.listen(port, () => {
---
+++ students/pei-tung/Canchu/util/redis.js
@@ -0,0 +1,12 @@
+const { createClient } = require('redis');
+
+const client = createClient();
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+client.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
+client.connect();
